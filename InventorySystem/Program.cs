76226a32004e77b66123806e2184e6265197864a
Leonardo Pérez <leo.prez.k@gmail.com>
2023-06-06 18:34:20 +0000

    Product prototype

--- InventorySystem/Program.cs
+++ InventorySystem/Program.cs
@@ -12,6 +12,17 @@ using static System.Console;
 
 namespace InventorySystem
 {
+    public class Product
+    {
+        public string Name { get; private set; }
+        public decimal Price { get; private set; }
+        public Product(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+
     public class Inventory
     {
         enum ProgramState
